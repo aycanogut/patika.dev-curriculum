fae3c4afb608cd35aba2050c84f6fee8b391cf5e
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-05 12:28:41 +0300

    card props declared to the component

--- react-odev-1/src/components/Card/Card.js
+++ react-odev-1/src/components/Card/Card.js
@@ -12,20 +12,20 @@ export default function Card(props) {
           <span className="letter">{props.author[0]}</span>
         </div>
         <div className="card-title-group">
-          <h5 className="card-title">Title</h5>
-          <div className="card-date">Date</div>
+          <h5 className="card-title">{props.item.title}</h5>
+          <div className="card-date">{props.item.date}</div>
         </div>
       </div>
       <img className="card-image" src={food} alt="Logo" />
-      <div className="card-text">Description</div>
+      <div className="card-text">{props.item.description}</div>
       <div className="card-like-bar">
-        {props.liked ? (
+        {props.isLiked ? (
           <img className="card-like-icon" src={heartFill} alt="Logo" />
         ) : (
           <img className="card-like-icon" src={heartOutline} alt="Logo" />
         )}
         <div className="like-text">
-          <b>7</b> kişi bu tarifi beğendi.
+          <b>{props.likeCount}</b> kişi bu tarifi beğendi.
         </div>
       </div>
     </div>
