ddd8a864b055b9e06451f37df91496d7512bb45b
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-05 12:28:03 +0300

    card props created

--- react-odev-1/src/App.js
+++ react-odev-1/src/App.js
@@ -12,15 +12,17 @@ function App() {
       "Seitan aynı zamanda 'buğday eti' olarak da anılır, piyasada bulunan birçok vegan ürünün temelidir. Genelde bunu öğütülmüş, dilimler veya şeritler gibi çeşitli stillerde bulabilirsiniz. Seitan, tam buğday unu veya yaşamsal buğday glüteni kullanılarak elle hazırlanabilir. Buğday hamurunun nişastasını durulayarak ve geride sadece yüksek proteinli glüten bırakarak yapılır.",
   };
 
-  const likeCount = 193;
-  const isLiked = false;
+  const likeCount = 666;
+  const isLiked = true;
 
   return (
     <div className="App">
       <header className="App-header">
         <Card
-          
           author={recipeAuthor}
+          item={recipeItem}
+          isLiked={isLiked}
+          likeCount={likeCount}
         />
       </header>
     </div>
