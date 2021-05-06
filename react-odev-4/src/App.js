f910ada21c623786e2e7b4e194b9244e91b098b5
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-06 20:48:45 +0300

    spaces fixed

--- react-odev-4/src/App.js
+++ react-odev-4/src/App.js
@@ -5,59 +5,63 @@ import { products } from "./constants";
 import ProductsGrid from "./components/ProductsGrid";
 
 const App = () => {
-   const [items, setItems] = useState({
-    cart: [
-      { id: 0, value: 0 },
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 },
-    ],
-    itemCount: 0,
-  });
+  const [item, setItem] = useState(
+    {
+      cart: [
+        { id: 0, value: 0 },
+        { id: 1, value: 0 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 },
+        { id: 4, value: 0 },
+        { id: 5, value: 0 },
+      ],
+      itemCount: 0,
+    }
+  );
 
   function handleIncrement(product) {
     // cart array'inin kopyasını oluştur
-    const cart = [...items.cart];
+    const cart = [...item.cart];
     // parametre olarak gelen product'ın cart array'i içerisindeki index'ini bul
-    const index = items.cart.indexOf(product);
+    const index = item.cart.indexOf(product);
     // kopyalanan cart array'ine bu ürünü ekle ve value property'sini 1 artır
-    cart[index] = { ...items.cart[index] };
+    cart[index] = { ...item.cart[index] };
     cart[index].value++;
     // getItemCount fonksiyonunu kullanarak sepetteki ürün sayısını bul
     const itemCount = getItemCount(cart);
     // state'i güncelle
-    setItems({ cart, itemCount });
+    setItem({ cart, itemCount });
   };
 
-  function handleDecrement(product)  {
-    const cart = [...items.cart];
-    const index = items.cart.indexOf(product);
-    cart[index] = { ...items.cart[index] };
+  function handleDecrement(product) {
+    const cart = [...item.cart];
+    const index = item.cart.indexOf(product);
+    cart[index] = { ...item.cart[index] };
     cart[index].value--;
     const itemCount = getItemCount(cart);
-    setItems({ cart, itemCount });
+    setItem({ cart, itemCount });
   };
 
   function getItemCount(cart) {
     // Sepetteki toplam ürün sayısını bul
-    let itemCount = cart.reduce((total, product) => total + product.value, 0);
+    let itemCount = cart.reduce((total, product) => total + product.value);
 
     return itemCount;
   };
 
+
     return (
       <div className="App">
-        <Navbar totalItems={items.itemCount} />
+        <Navbar totalItems={item.itemCount} />
         <ProductsGrid
           products={products}
-          cart={items.cart}
+          cart={item.cart}
           onIncrement={handleIncrement}
           onDecrement={handleDecrement}
         />
       </div>
     );
-}
+  }
+
 
 export default App;
