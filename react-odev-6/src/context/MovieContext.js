e059237411c0e7e28bb70d47da49eb273347cc9f
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-07 15:17:54 +0300

    API_KEY declared to MovieContext.js

--- react-odev-6/src/context/MovieContext.js
+++ react-odev-6/src/context/MovieContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 export const MovieContext = createContext();
 
-const API_KEY = ''; // OMDb API Key
+const API_KEY = 'e6f362ad'; // OMDb API Key
 
 const MovieApp = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
