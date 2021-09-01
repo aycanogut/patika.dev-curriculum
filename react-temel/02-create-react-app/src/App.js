664bf8c2a6956571194ba2f9a0d6736d70bf974f
Aycan Öğüt <aycanonfire@gmail.com>
2021-09-01 12:09:37 +0300

    updated / create-react-app path

--- react-temel/02-create-react-app/src/App.js
+++ react-temel/02-create-react-app/src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 
+const name = 'Aycan';
+const age = 30;
+const isLoggedIn = true;
+
+
 function App() {
   return (
     <div className="App">
+      <h1>{isLoggedIn ?  `${name} and ${age}` : 'logged out'}</h1>
+      
      <Header />
     </div>
   );
