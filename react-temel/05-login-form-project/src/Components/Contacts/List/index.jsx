6f6f4733a2e457231ee2cde592a5445041d04bff
aycanogut <aycanonfire@gmail.com>
2021-09-13 12:12:11 +0300

    Add list preview functionality and some styles

--- react-temel/05-login-form-project/src/Components/Contacts/List/index.jsx
+++ react-temel/05-login-form-project/src/Components/Contacts/List/index.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-// eslint-disable-next-line react/prop-types
 function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => {
+      return item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase());
+    });
+  });
+
   return (
     <header>
       <h1>Contacts</h1>
+
+      <input
+        type="text"
+        placeholder="Filter contacts"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+
       <ul>
-        {contacts.map((contact, index) => (
+        {filtered.map((contact, index) => (
           <li key={index}>
             {contact.fullName}, {contact.phoneNumber}
           </li>
--- react-temel/05-login-form-project/src/Components/Contacts/index.jsx
+++ react-temel/05-login-form-project/src/Components/Contacts/index.jsx
@@ -3,15 +3,28 @@ import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullName: "Aycan",
+      phoneNumber: "123432",
+    },
+    {
+      fullName: "Selin",
+      phoneNumber: "76543",
+    },
+    {
+      fullName: "Elvan",
+      phoneNumber: "395982",
+    },
+  ]);
 
   useEffect(() => {}, [contacts]);
 
   return (
-    <>
+    <div className="container">
       <List contacts={contacts} />
       <Form addContact={setContacts} contacts={contacts} />
-    </>
+    </div>
   );
 }
 
